59bdff8c73f8baf2bbf75af011bc8737ad5ff60f
Darshan Bagade <darshanbagade123@gmail.com>
2025-10-29 22:25:46 +0530

    Add MyArrayList class with basic ArrayList operations

---
+++ Eagles/Session-01/ArrayList.java
@@ -0,0 +1,16 @@
+public class MyArrayList {
+    public static void main(String[] args) {
+        // ArrayList: Dynamic array size
+
+        java.util.ArrayList<Integer> t = new java.util.ArrayList<Integer>();
+        
+        t.add(11);
+        t.add(51);
+        t.add(14);
+        t.set(0, 5);//insert Element at 0 position
+    }
+}
+
+//ArrayList < LinkedList
+
+//LinkedList
